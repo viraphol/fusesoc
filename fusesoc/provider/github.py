84e7680a0869d35a795d7d986d569d5800f8f148
viraphol <virapholchaiyakul@gmail.com>
2021-02-16 16:31:44 -0800

    add capability to provide githup access token so design data can be protected.  Check vdec/pair1 example

--- fusesoc/provider/github.py
+++ fusesoc/provider/github.py
@@ -6,6 +6,7 @@ import logging
 import os.path
 import sys
 import tarfile
+import tempfile
 
 from fusesoc.provider.provider import Provider
 
@@ -21,21 +22,46 @@ else:
 
 URL = "https://github.com/{user}/{repo}/archive/{version}.tar.gz"
 
-
 class Github(Provider):
     def _checkout(self, local_dir):
         user = self.config.get("user")
         repo = self.config.get("repo")
+        token = self.config.get("token")
 
         version = self.config.get("version", "master")
 
         # TODO : Sanitize URL
         url = URL.format(user=user, repo=repo, version=version)
         logger.info("Downloading {}/{} from github".format(user, repo))
-        try:
-            (filename, headers) = urllib.urlretrieve(url)
-        except URLError as e:
-            raise RuntimeError("Failed to download '{}'. '{}'".format(url, e.reason))
+        logger.info("URL: {}".format(url))
+
+        # if authentication token is given use it
+        if (token)  :
+            r = urllib.Request(url)
+            r.add_header('Authorization', ("token " + token))
+            #r = urllib.Request("https://github.com/viraphol/pair1/archive/v1.1.tar.gz")
+            #r.add_header('Authorization', "token 79a783b2d2c13bedcd458f599abf61670bbd0063")
+            try:
+                with urllib.urlopen(r) as resp:
+                    fd, filename = tempfile.mkstemp()
+                    try:
+                        with os.fdopen(fd, 'wb') as out:
+                            data = resp.read()
+                            if data:
+                                out.write(data)
+                    except:
+                        raise RuntimeError("Failed to write to '{}'".format(filename))
+            except URLError as e:
+                raise RuntimeError("Failed to download '{}'. '{}'".format(url, e.reason))
+        else:
+            try:
+                (filename, headers) = urllib.urlretrieve(url)
+            except URLError as e:
+                raise RuntimeError("Failed to download '{}'. '{}'".format(url, e.reason))
+
+
+        logger.info("filename: {}".format(filename))
+
         t = tarfile.open(filename)
         (cache_root, core) = os.path.split(local_dir)
 
